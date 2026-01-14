2ef61f0bbdb6a3bcbac1f5ce409d84e79f73ac49
Chikwa235 <chisapachikwa25@gmail.com>
2026-01-14 07:18:27 +0200

    Add basic layout for AI Product Image Generator page

--- ai-ad/app/(routes)/creative-ai-tools/product-images/page.tsx
+++ ai-ad/app/(routes)/creative-ai-tools/product-images/page.tsx
@@ -3,7 +3,15 @@ import React from 'react'
 function ProductImages() {
   return (
     <div>
-      <div>ProductImages</div>
+      <h2>AI Product Image Generator</h2>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+        <div>
+          Form
+        </div>
+        <div className="md:grid-cols-2">
+          Preview
+        </div>
+      </div>
     </div>
   )
 }
